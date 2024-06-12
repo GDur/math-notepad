470cfc2a2177d6f92ded90663ad21158cb5df85e
GDur <gregor.mugen@gmail.com>
2024-06-12 07:31:35 +0200

    feature: added clean MY_MATH_UNITS

---
+++ src/hljs/MY_MATH_UNITS.ts
@@ -0,0 +1,77 @@
+export const MY_MATH_UNITS = {
+    'USD': {
+        "name": "USD",
+
+        "base": {
+            "dimensions": [
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            "key": "CURRENCY"
+        },
+        "prefixes": {
+            "": {
+                "name": "",
+                "value": 1,
+                "scientific": true
+            },
+        },
+        "value": 1,
+        "offset": 0,
+        "dimensions": [
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+    "EUR": {
+        "name": "EUR",
+        "base": {
+            "dimensions": [
+                0,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            "key": "CURRENCY"
+        },
+        "prefixes": {
+            "": {
+                "name": "",
+                "value": 1,
+                "scientific": true
+            },
+        },
+        "value": 1,
+        "offset": 0,
+        "dimensions": [
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ]
+    },
+}
